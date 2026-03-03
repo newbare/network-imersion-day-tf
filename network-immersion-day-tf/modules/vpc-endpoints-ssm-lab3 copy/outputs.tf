846d79007be9506ce90bbea3c3c450442c23dea2
jefferson.silva <jefferson.silva@spassu.com.bnr>
2026-03-03 02:31:45 -0300

    fix(vpc-endpoints-ssm-lab3): corrige sintaxe no arquivo outputs.tf

---
+++ network-immersion-day-tf/modules/vpc-endpoints-ssm-lab3 copy/outputs.tf
@@ -0,0 +1,11 @@
+# modules/vps-ssm-lab3/outputs.tf
+
+output "ssm_endpoint_id" {
+  value = aws_vpc_endpoint.ssm.id
+}
+output "ssmmessages_endpoint_id" {
+  value = aws_vpc_endpoint.ssmmessages.id
+}
+output "ec2messages_endpoint_id" {
+  value = aws_vpc_endpoint.ec2messages.id
+}
